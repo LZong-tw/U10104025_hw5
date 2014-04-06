d9241e8dfa338eae69183d86c5fb71a3b7566d97
LZong <lzong.tw@gmail.com>
2014-04-06 11:55:48 +0800

    新增
    製作密碼檢查的class，以及計算機主畫面

--- src/Check.java
+++ src/Check.java
@@ -1,19 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-import javax.swing.JFrame;
-import java.util.Scanner;
-
-public class Check{
-	static String passwd=new String("0000");
-	static Scanner ipt=new Scanner(System.in);
-	public static void main(String[] args){
-		JFrame check=new JFrame("check");
-		while(!ipt.equals(passwd)){
-			check.setSize(800, 800);
-			check.setLocationRelativeTo(null);
-			check.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			check.setVisible(true);
-		}
-		check.setVisible(false);
-		Main;
+public class Check extends JFrame{	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5504430587599356467L;
+	static JPasswordField passwd=new JPasswordField();
+	public Check(){
+		JPanel pl=new JPanel();
+		JButton CheckButton=new JButton("Check");
+		pl.setLayout(new GridLayout(1,1));
+		pl.add(CheckButton);
+		CheckButton.addActionListener(new CheckHandler());
+		
+		JPanel p2=new JPanel(new BorderLayout());
+		p2.add(new JLabel("Password:"),BorderLayout.NORTH);
+		p2.add(passwd,BorderLayout.CENTER);
+		p2.add(pl,BorderLayout.SOUTH);
+		
+		add(p2,BorderLayout.CENTER);
 	}
+	
+	static String pass=new String("0000");
+	static Check check=new Check();
+	public static void main(String[] args){
+		
+		check.setTitle("Check Password");
+		check.setSize(200, 120);
+		check.setLocationRelativeTo(null);
+		check.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		check.setVisible(true);
+	}
+	
+	public class CheckHandler implements ActionListener {
+		public void actionPerformed(ActionEvent e){
+			String psswd=new String(passwd.getPassword());
+			if(psswd.equals(pass)){
+				check.setVisible(false);
+			}
+		}
+	}
+	
+	
 }
